576ee4970558a146296d20c57114a1c860983a7c
Max Metral <max@pyralis.com>
2017-08-17 10:05:04 -0400

    Update to latest of the various modules

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb',
   rules: {
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': ['warn', { aspects: ['invalidHref'] }],
     'import/prefer-default-export': [0],
     'no-restricted-syntax': [
       'error',
       'ForInStatement',
       'LabeledStatement',
-      'WithStatement'
+      'WithStatement',
     ],
-    'react/jsx-filename-extension': [1, { "extensions": ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'no-param-reassign': [
       2,
       {
         props: false,
-      }
+      },
     ],
   },
-}
+};
