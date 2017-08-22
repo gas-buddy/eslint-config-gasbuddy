afd303f0cd0b5ef61c476ba5d618ff8d3f75120d
Max Metral <max@pyralis.com>
2017-08-22 16:39:31 -0400

    Monkey patch the s**t out of require because eslint/npm5 aren't friends

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// We need to do this horrible thing because of the interaction of module resolution and npm 5.
+// We want our dependencies to just depend on eslint-config-gasbuddy and be done, but
+// this doesn't work if the binary is running from the referencing package because npm5 buries our
+// dependencies under eslint-config-gasbuddy node_modules, where eslint will not find them.
+var monkeypatch = require('monkeypatch');
+
+var replacements = {
+  'eslint-plugin-jsx-a11y': require('eslint-plugin-jsx-a11y'),
+  'eslint-plugin-react': require('eslint-plugin-react'),
+  'eslint-plugin-import': require('eslint-plugin-import'),
+};
+
+monkeypatch(require('module').prototype, 'require', function (original, modname) {
+  if (replacements[modname]) {
+    return replacements[modname];
+  }
+  return original(modname);
+});
+
 module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb',
