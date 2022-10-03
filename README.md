ff0ba4778ee6d7e34db823e7df005aedc84a4917
Max Metral <max@pyralis.com>
2022-10-03 15:12:19 -0400

    Completely new config - based on Typescript

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # eslint-config-gasbuddy
 
-[![wercker status](https://app.wercker.com/status/fe95b7eb84eda02480c380dedd47a1fb/s/master "wercker status")](https://app.wercker.com/project/byKey/fe95b7eb84eda02480c380dedd47a1fb)
+GasBuddy opinions about Javascript and Typescript (ok, so it's airbnb
+minus some stuff). Mainly here to provide an abstraction for all our
+projects that allows updated rules to propagate more easily and under our control.
 
-GasBuddy opinions about Javascript (ok, so it's airbnb minus some stuff). Mainly
-here to provide an abstraction for all our projects that allows updated rules
-to propagate more easily and under our control.
-
-## V6.1.0
+## V7.0.0
 
 The main changes in this version are changes to the arrow-parens rule.  We also updated the underlying libraries including updating ESLint v5 -> v6.
--- index.js
+++ index.js
@@ -1,59 +1,13 @@
-// We need to do this horrible thing because of the interaction of module resolution and npm 5.
-// We want our dependencies to just depend on eslint-config-gasbuddy and be done, but
-// this doesn't work if the binary is running from the referencing package because npm5 buries our
-// dependencies under eslint-config-gasbuddy node_modules, where eslint will not find them.
-var monkeypatch = require('monkeypatch');
-
-var replacements = {
-  'eslint-plugin-jsx-a11y': require('eslint-plugin-jsx-a11y'),
-  'eslint-plugin-react': require('eslint-plugin-react'),
-  'eslint-plugin-import': require('eslint-plugin-import'),
-  'eslint-plugin-react-hooks': require('eslint-plugin-react-hooks'),
-};
-
-monkeypatch(require('module').prototype, 'require', function (original, modname) {
-  if (replacements[modname]) {
-    return replacements[modname];
-  }
-  return original(modname);
-});
-
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
-  plugins: ['babel', 'react-hooks'],
-  globals: {
-    BigInt: true
+  root: true,
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
   },
   rules: {
-    camelcase: 0,
-    'babel/camelcase': ['error', { properties: 'never' }],
-    'max-len': ['error', 160, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-    'jsx-a11y/href-no-hash': 'off',
-    'object-curly-newline': 'off',
-    'jsx-a11y/anchor-is-valid': ['warn', { aspects: ['invalidHref'] }],
     'import/prefer-default-export': [0],
-    'no-restricted-syntax': [
-      'error',
-      'ForInStatement',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/no-did-update-set-state': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'no-param-reassign': [
-      2,
-      {
-        props: false,
-      },
-    ],
   },
 };
