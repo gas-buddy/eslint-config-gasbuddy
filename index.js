9ef16466c58bdd34fbc304bf2abfe6d552ad2ce1
Dhruv Patel <dhruv.patel@pdisoftware.com>
2024-01-17 14:52:07 -0600

    refactor: Make configuration flexible to support both js and ts projects

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ module.exports = {
   root: true,
   extends: [
     'airbnb-base',
-    'airbnb-typescript/base',
   ],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      plugins: ['@typescript-eslint'],
+    },
+  ],
   rules: {
     'import/prefer-default-export': [0],
     'no-param-reassign': [
